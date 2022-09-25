6d1d495e8f1381e9e1f0fea8c31c06f94dda86ca
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-09-25 16:26:08 +0900

    Update 문자열 내 p와 y의 개수.java

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -6,11 +6,11 @@ class Solution {
         char ch = ' ';
         
         for (int i = 0; i < s.length(); i++) {
-			ch = s.charAt(i);
-			if(ch == 'p' || ch == 'P') {
-                cntP++;
-            } else if (ch == 'y' || ch == 'Y') {
-                cntY++;
+		ch = s.charAt(i);
+		if(ch == 'p' || ch == 'P') {
+                 cntP++;
+               } else if (ch == 'y' || ch == 'Y') {
+                 cntY++;
             }
         }
         if (cntP == cntY) {
