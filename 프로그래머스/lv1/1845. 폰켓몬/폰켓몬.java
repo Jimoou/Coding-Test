bbb28cb20cca7ceb7c1bb6f7acf701a96b776203
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-09-28 17:46:50 +0900

    [level 1] Title: 폰켓몬, Time: 3.39 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int cnt = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            set.add(n);
+        }
+        if (set.size() > nums.length/2) {
+            return nums.length/2;
+        } else {
+            return set.size();
+        }
+    }
+}
