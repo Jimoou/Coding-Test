62d855ae9fe1edd83ed84b8eb48627479dc462f1
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-09-30 22:12:55 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.java
@@ -1,35 +1,26 @@
+import java.util.Arrays;
 class Solution {
-   public int solution(int[] nums) {
-      int answer = 0;
-      boolean chk = false;
-
-      for (int i = 0; i < nums.length; i++) {
-         for (int j = i + 1; j < nums.length; j++) {
-            for (int k = j + 1; k < nums.length; k++) {
-               int num = nums[i] + nums[j] + nums[k]; //값을 담아
-               if (num >= 2) //경우의 수를 찾아서
-                  chk = sosu(num);
-               if (chk == true) //소수가 맞을 경우
-                  answer++; 
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for(int i = 0; i < nums.length - 2; i ++){
+            for(int j = i + 1; j < nums.length - 1; j ++){
+                for(int k = j + 1; k < nums.length; k ++ ){
+                    if(isPrime(nums[i] + nums[j] + nums[k])){
+                        answer++;  
+                    } 
+                }
             }
-         }
-
-      }
-      return answer;
-   }
-   
-   public boolean sosu(int num) {
-      boolean check = true; 
-      if(num==2) { //소수일때
-         return check;
-         
-      }
-      for(int i=2; i<num; i++) { //소수가 아닐때
-         if(num%i ==0) {
-            check = false;
-            break;
-         }
-      }
-      return check;
-   }
+        }
+        return answer;
+    }
+    public Boolean isPrime(int num){
+        int cnt = 0;
+        for(int i = 1; i <= (int)Math.sqrt(num); i++){
+            if(num % i == 0) {
+                cnt++;
+            }
+        }
+        return cnt == 1;
+    }
 }
