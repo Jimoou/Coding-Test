284e2492cb862f67e11863644b4aa6dbd3b52899
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-05 14:45:31 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.24 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String solution(int[] numbers, String hand) {
+        StringBuilder sb = new StringBuilder();
+        int left = 10;
+        int right = 12;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == 1 ||numbers[i] == 4 || numbers[i] == 7) {
+                left = numbers[i];
+                sb.append("L");
+            } else if (numbers[i] == 3 ||numbers[i] == 6 || numbers[i] == 9) {
+                right = numbers[i];
+                sb.append("R");
+            } else {
+                if(numbers[i] == 0) {
+                    numbers[i] = 11;
+                }
+                int leftDistance = Math.abs(numbers[i]-left)/3+Math.abs(numbers[i]-left)%3;
+                int rightDistance = Math.abs(numbers[i]-right)/3+Math.abs(numbers[i]-right)%3;
+                if (leftDistance > rightDistance) {
+                    sb.append("R");
+                    right = numbers[i];
+                } else if (leftDistance < rightDistance) {
+                    sb.append("L");
+                    left = numbers[i];
+                } else {
+                    if(hand.equals("left")) {
+                        sb.append("L");
+                        left = numbers[i];
+                    } else {
+                        sb.append("R");
+                        right = numbers[i];
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
