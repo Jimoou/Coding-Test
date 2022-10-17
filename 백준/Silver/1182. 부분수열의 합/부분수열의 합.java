c292e0541ca4701b7da5cf2fe76be348242c1df1
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-17 13:28:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 216 ms, Memory: 17756 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    static int N, S, ans;
+    static int[] nums;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        S = sc.nextInt();
+        nums = new int[N+1];
+        for (int i = 1; i <= N; i++) nums[i] = sc.nextInt();
+
+        rec_func(1, 0);
+        if (S == 0) {
+            ans--;
+        }
+        System.out.println(ans);
+    }
+    static void rec_func (int k, int value) {
+        if (k == N + 1) {
+            if (value == S) ans++;
+        } else {
+            rec_func(k + 1, value + nums[k]);
+            rec_func(k + 1, value);
+        }
+    }
+
+}
