cde9e70c6e79e7d8d6e7026ea06baba21a3acac3
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-25 11:08:33 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,22 +1,2 @@
 def solution(strings, n):
-    if len(strings) <= 1:
-        return strings
-    right = []
-    left = []
-    pivot = strings[0]
-    for i in range (1, len(strings)):
-        if strings[i][n] > pivot[n]:
-            right.append(strings[i])
-        elif strings[i][n] == pivot[n]:
-            if strings[i] > pivot:
-                right.append(strings[i])
-            else:
-                left.append(strings[i])
-        else:
-            left.append(strings[i])
-
-    answer = []
-    answer += solution(left, n)
-    answer.append(pivot)
-    answer += solution(right, n)
-    return answer
+    return sorted(sorted(strings), key=lambda x:x[n])
