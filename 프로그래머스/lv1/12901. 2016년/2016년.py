0e332260a609ab1e44408f10c0159c0fa43ed0e8
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-27 14:14:08 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.py
+++ 프로그래머스/lv1/12901. 2016년/2016년.py
@@ -2,4 +2,4 @@ def solution(a, b):
     day = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
     date = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    return day[(sum(date[:a-1])+b+4)%7]
+    return day[(sum(date[:a-1])+b-3)%7]
