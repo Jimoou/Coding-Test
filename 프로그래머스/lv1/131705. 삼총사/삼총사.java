f9130a4cad33af29752fa3ea787372ef2527db25
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-27 13:05:13 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -4,22 +4,12 @@ class Solution {
         for (int i = 0; i < number.length-2; i++) {
             for (int j = i+1; j < number.length-1; j++) {
                 for (int k = j+1; k < number.length; k++) {
-                    if (0 == additionNum(number, i, j, k)) {
+                    if (0 == number[i] + number[j] + number[k]) {
                         answer++;
                     }
                 }
             }
         }
         return answer;
-    }
-
-    static int additionNum(int[] number, int n1, int n2, int n3){
-        int sum = 0;
-        for (int i = 0; i < number.length; i++) {
-            if (i == n1 || i == n2 || i == n3) {
-                sum += number[i];
-            }
-        }
-        return sum;
     }
 }
