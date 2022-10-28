6407dc584b1fb99d6ad304ac5f9bfbdd71df55ae
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-28 18:05:55 +0900

    [level 1] Title: 폰켓몬, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,7 +1,2 @@
 def solution(nums):
-    if len(nums)//2 <= int(len(set(nums))):
-        return len(nums)//2
-    elif len(nums)//2 >= int(len(set(nums))):
-        return int(len(set(nums)))
-    else:
-        return 1
+    return min(len(nums)/2, len(set(nums)))
