6148f0fe6887d2644354eed02892d3e666f7e669
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-30 14:39:32 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.py
@@ -1,16 +1,10 @@
-import math
-
-
 def solution(nums):
+    from itertools import combinations as cb
     answer = 0
-    num = []
-    for i in range (len(nums)):
-        for j in range (i+1,len(nums)):
-            for k in range (j+1,len(nums)):
-                num.append(nums[i] + nums[j] + nums[k])
-    for i in num:
-        for k in range (2, int(math.sqrt(i)) + 1):
-            if bool(i % k == 0):
+    for a in cb(nums, 3):
+        cand = sum(a)
+        for j in range(2, cand):
+            if cand%j==0:
                 break
         else:
             answer += 1
