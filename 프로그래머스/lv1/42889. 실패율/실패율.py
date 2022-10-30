813431720345bc786abf38ec218860069a048916
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-10-30 15:33:46 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -0,0 +1,13 @@
+def solution(N, stages):
+
+    di = dict(zip(range(1,N+1), range(N)))
+    temp = 0
+    for i in range (1, N+1):
+        if stages.count(i) != 0:
+            di[i] = stages.count(i)/(len(stages)-temp)
+        else:
+            di[i] = 0
+        temp += stages.count(i)
+
+
+    return sorted(di, key=lambda x:di[x], reverse=True)
