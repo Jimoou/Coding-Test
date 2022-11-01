a66acace6270340253af8587608ddcfdd0b53867
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-01 21:10:12 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.85 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.78 ms
+메모리: 78.3 MB, 시간: 0.85 ms
 
 ### 구분
 
--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -2,35 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        int dollNum = 0;
-        int temp = 0;
+        int count = 0;
         Stack<Integer> bucket = new Stack<>();
-        
-        for (int i = 0; i < moves.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (board[j][moves[i] - 1] != 0) {
-                    dollNum = board[j][moves[i] - 1];
-                    bucket.push(board[j][moves[i] - 1]);
-                    board[j][moves[i] - 1] = 0;
+        for (int i : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][i-1] != 0) {
+                    if (bucket.size() > 0) {
+                        if (bucket.peek() == board[j][i-1]) {
+                            bucket.pop();
+                            count += 2;
+                        } else {
+                            bucket.push(board[j][i-1]);
+                        }
+                    }
+                    else {bucket.push(board[j][i - 1]);}
+                    board[j][i-1] = 0;
                     break;
-                } else if (board[board[0].length - 1][moves[i] - 1] == 0) {
-                    dollNum = 0;
-                    temp--;
-                    break;
-                }
-            }
-            temp++;
-            if (bucket.size() > 1) {
-                if (bucket.get(temp - 2) == dollNum && dollNum != 0) {
-                    bucket.pop();
-                    bucket.pop();
-                    temp -= 2;
-                    answer += 2;
                 }
             }
         }
-
-        return answer;
+        return count;
     }
 }
