969c2c12cff546ce5ad20eaa6b0c35073629f228
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-02 10:33:31 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.21 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,10 @@
+import re
+def solution(new_id):
+    answer = new_id.lower()
+    answer = re.sub("[^a-z0-9\-_.]","", answer)
+    answer = re.sub("\.+", ".", answer)
+    answer = re.sub("^[.]|[.]$", "", answer)
+    answer = 'a' if len(answer) == 0 else answer[:15]
+    answer = re.sub("\.$", "", answer)
+    while len(answer) < 3: answer += answer[-1]
+    return answer
