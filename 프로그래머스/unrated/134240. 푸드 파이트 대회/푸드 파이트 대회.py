e23f451cab764dc2c3db2d73793b8cd1d39b4dc3
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-04 13:44:09 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,8 @@
 def solution(food):
-    left_Line = []
+    left_Line = ''
     for i in range(1,len(food)):
         if food[i] > 1:
-            left_Line.append(str(i)*(food[i]//2))
+            left_Line += str(i)*(food[i]//2)
     right_Line = left_Line[::-1]
-    left_Line.append(0)
-    return ''.join(map(str,(left_Line+right_Line)))
+    left_Line += '0'
+    return left_Line + right_Line
