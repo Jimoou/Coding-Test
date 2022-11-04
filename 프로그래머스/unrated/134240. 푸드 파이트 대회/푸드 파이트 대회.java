31a697cdf0d93759754ef2d701bcbaa2e96768c1
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-04 14:05:51 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 16.34 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder left_Line = new StringBuilder();
-        StringBuilder right_Line = new StringBuilder();
         for (int i = 1; i < food.length; i++) {
             if (food[i] > 1) {
-                left_Line.append(String.valueOf(i).repeat(food[i]/2));
-                right_Line.append(String.valueOf(i).repeat(food[i]/2));
+                left_Line.append(String.valueOf(i).repeat(food[i] / 2));
             }
         }
-        left_Line.append(0);
-        return left_Line.toString() + right_Line.reverse();
+        return left_Line.toString()+"0"+left_Line.reverse().toString();
     }
 }
