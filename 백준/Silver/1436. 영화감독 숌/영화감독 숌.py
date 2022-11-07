325e9bc3546aff94f08322e161f19471f251b6c1
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-07 11:23:54 +0900

    [Silver V] Title: 영화감독 숌, Time: 780 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,25 +1,8 @@
-def count6(n):
-    cnt = 0
-    while n > 0:
-        if n % 10 == 6:
-            cnt += 1
-        else:
-            cnt = 0
-        if cnt >= 3:
-            return True
-        n = n // 10
-
-    return False
-
-
 if __name__ == "__main__":
     N = int(input())
-
     answer = 665
-
     while N > 0:
         answer += 1
-        if count6(answer):
+        if '666' in str(answer):
             N -= 1
-
     print(answer)
