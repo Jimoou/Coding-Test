47194a6a69a0647747206bcc001b842155859e8b
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-11-11 05:22:35 +0900

    [unrated] Title: 과일 장수, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -2,7 +2,6 @@ def solution(k, m, score):
     score.sort()
 
     sum = 0
-    for packed in range(m,len(score)+1,m):
-        box = list(score[-packed:len(score)-packed+m])
-        sum += min(box)*m
+    for packed in range(len(score),m-1, -m):
+        sum += score[packed-m]*m
     return sum
