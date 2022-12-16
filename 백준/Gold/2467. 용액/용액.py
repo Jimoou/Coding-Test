e3cac1113895abae82798cafe2c675625e3746de
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2022-12-16 10:10:29 +0900

    [Gold V] Title: 용액, Time: 132 ms, Memory: 41208 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -5,19 +5,17 @@ if __name__ == '__main__':
     A = list(map(int, sys.stdin.readline().split()))
 
     start = 0
-    end = N - 1
+    end = N-1
 
-    max_sum = sys.maxsize
+    minSum = sys.maxsize
 
     liquid1 = 0
     liquid2 = 0
 
-    A.sort()
-
     while start < end:
         curSum = A[start] + A[end]
-        if abs(curSum) < max_sum:
-            max_sum = abs(curSum)
+        if abs(curSum) < minSum:
+            minSum = abs(curSum)
             liquid1 = A[start]
             liquid2 = A[end]
 
@@ -25,5 +23,4 @@ if __name__ == '__main__':
             end -= 1
         else:
             start += 1
-
     print(liquid1, liquid2)
