6915edaba5b72adb1c8fe7ec24110239d6122b9d
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2023-01-10 18:52:06 +0900

    [Silver V] Title: 영화감독 숌, Time: 544 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,8 +1,12 @@
-if __name__ == "__main__":
-    N = int(input())
-    answer = 665
-    while N > 0:
-        answer += 1
-        if '666' in str(answer):
-            N -= 1
-    print(answer)
+import sys
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline())
+    max = sys.maxsize
+    cnt = 0
+    for i in range(max):
+        if "666" in str(i):
+            cnt += 1
+            if cnt == N:
+                print(i)
+                break
