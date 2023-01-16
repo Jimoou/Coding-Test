0a3cc55f671a9748d6396c04737209d00ec485b3
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2023-01-16 19:32:41 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 184 ms, Memory: 119940 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -1,24 +1,26 @@
 import sys
 
-T = int(input())
-for t in range(T):
-    N, M = map(int, sys.stdin.readline().split())
+if __name__ == '__main__':
+    T = int(sys.stdin.readline())
 
-    A = list(map(int, sys.stdin.readline().split()))
-    B = list(map(int, sys.stdin.readline().split()))
-    B.sort()
+    for _ in range(T):
+        N, M = map(int, sys.stdin.readline().split())
+        A = list(map(int, sys.stdin.readline().split()))
+        B = list(map(int, sys.stdin.readline().split()))
 
-    count = 0
-    for i in range(0,N):
-        L = 0
-        R = M-1
-        result = -1
-        while L <= R:
-            mid = (L + R) // 2
-            if B[mid] < A[i]:
-                result = mid
-                L = mid + 1
-            else:
-                R = mid - 1
-        count += result + 1
-    print(count)
+        B.sort()
+
+        cnt = 0
+        for a in A:
+            L = 0
+            R = M - 1
+            result = -1
+            while L <= R:
+                mid = (L + R) // 2
+                if B[mid] < a:
+                    result = mid
+                    L = mid + 1
+                else:
+                    R = mid - 1
+            cnt += result + 1
+        print(cnt)
