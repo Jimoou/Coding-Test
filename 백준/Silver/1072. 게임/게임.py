b553815255413c0cfc5441abf2c97c89051394cb
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2023-01-17 09:44:47 +0900

    [Silver III] Title: 게임, Time: 128 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.py
+++ 백준/Silver/1072. 게임/게임.py
@@ -2,17 +2,18 @@ import sys
 
 if __name__ == '__main__':
     X, Y = map(int, sys.stdin.readline().split())
-    Z = int((Y*100) / X)
-    L = 0
-    R = 1000000000
-    mid = 0
+    Z = int((Y * 100 / X))
+
+    start = 0
+    end = 1000000000
+
     if Z >= 99:
         print(-1)
     else:
-        while L <= R:
-            mid = (L + R) // 2
+        while start <= end:
+            mid = (start + end) // 2
             if int(((Y + mid) * 100) / (X + mid)) <= Z:
-                L = mid + 1
+                start = mid + 1
             else:
-                R = mid - 1
-        print(L)
+                end = mid -1
+        print(start)
