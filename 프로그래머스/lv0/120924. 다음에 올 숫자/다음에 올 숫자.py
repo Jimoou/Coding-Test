3e7e65e8c3875ee274a4cab4c3a5bcbb5f45f4da
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2023-01-31 19:26:55 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.py
@@ -1,8 +1,3 @@
 def solution(common):
     gap = common[1] - common[0]
-    if common[1] + gap == common[2]:
-        answer = common[len(common) - 1] + gap
-    else:
-        gap = common[1] // common[0]
-        answer = common[len(common) - 1] * gap
-    return answer
+    return common[len(common) - 1] + gap if common[1] + gap == common[2] else common[len(common) - 1] * (common[1] // common[0])
