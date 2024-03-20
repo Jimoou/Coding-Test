2e732aa3c985e8c06437f8207d725d88efc8ff92
Jiwoon <109801772+Jimoou@users.noreply.github.com>
2024-03-20 15:48:37 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.12 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -1,38 +1,37 @@
 class Solution {
     public int solution(int[] bandage, int health, int[][] attacks) {
+        int currentHP = health;
+        int currentTime = 1;
+        int attackIndex = 0;
+        int continuous = 0;
         
-    int currentHP = health;
-    int lastAttackTime = attacks[attacks.length - 1][0];
-    int countinous = 0;
-    boolean attacked = false;
-
-    int t = 1;
-    while (t <= lastAttackTime) {
-      for (int[] attack : attacks) {
-        if (t == attack[0]) {
-          currentHP -= attack[1];
-          if (currentHP <= 0) {
-            return -1;
-          }
-          attacked = true;
+        while (true) {
+            boolean attacked = false;
+            
+            if (attackIndex < attacks.length && currentTime == attacks[attackIndex][0]) {
+                currentHP -= attacks[attackIndex][1];
+                if (currentHP <= 0) return -1;
+                attackIndex++;
+                attacked = true;
+                continuous = 0;
+            }
+            if (!attacked) {
+                if (continuous < bandage[0]) {
+                    currentHP += bandage[1];
+                    continuous++;
+                }
+                if (continuous == bandage[0]) {
+                    currentHP += bandage[2];
+                    continuous = 0;
+                }
+                if (currentHP > health) currentHP = health;
+            }
+            if (attackIndex >= attacks.length) {
+                if (continuous == 0) break;
+            } else {
+                currentTime++;
+            }
         }
-      }
-      if (attacked) {
-        countinous = 0;
-      } else {
-        currentHP += bandage[1];
-        countinous++;
-        if (countinous == bandage[0]) {
-          currentHP += bandage[2];
-          countinous = 0;
-        }
-        if (currentHP > health) {
-          currentHP = health;
-        }
-      }
-      attacked = false;
-      t++;
+        return currentHP;
     }
-    return currentHP;
-  }
 }
